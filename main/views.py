194b18e7953aaababea30915180549238fa1e6e0
hyunji5252 <ckddbs1682@gmail.com>
2023-07-28 23:44:12 +0900

    pythonanywhere 연동 확인

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from random import *
+from .models import *
+from django.db import connection
+from django.urls import reverse
+from django.db.models import Q # Q는 Django내 Model을 관리할 때 사용되는 ORM으로 SQL의 WHERE절과 같은 조건문을 추가할 때 사용한다.
+#from .forms import *
+from datetime import datetime
 
-# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("Hello, World. You're at the polls index.")
